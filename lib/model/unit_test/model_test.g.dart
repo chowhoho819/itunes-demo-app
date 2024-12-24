9db93dbd13f16e3473ed20489775dc8b10cab1ec
Ricky Chow <rickychow.48852@gmail.com>
2024-12-24 14:47:10 +0800

    test on freezed serializer

---
+++ lib/model/unit_test/model_test.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'model_test.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ModelTestImpl _$$ModelTestImplFromJson(Map<String, dynamic> json) =>
+    _$ModelTestImpl(
+      id: json['id'] as String,
+      name: json['name'] as String?,
+    );
+
+Map<String, dynamic> _$$ModelTestImplToJson(_$ModelTestImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+    };
