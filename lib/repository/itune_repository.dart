fdb63556242cf0822c9bf427bbe4035d18a3b4da
Ricky Chow <rickychow.48852@gmail.com>
2024-12-24 14:39:34 +0800

    Added json serializer for further object casing

--- lib/repository/itune_repository.dart
+++ lib/repository/itune_repository.dart
@@ -1,1 +1,11 @@
+import 'package:itune_test_app/model/itune_res.dart';
+import 'package:itune_test_app/utils/dio_client.dart';
 
+class ItuneRepository {
+  DioClient client = DioClient();
+
+  Future<void> getSongs({Map<String, dynamic>? queryParameters}) async {
+    final path = "https://itunes.apple.com/search?term=Talyor+Swift&limit=10&media=music";
+    await client.get<ItuneResponse>(path);
+  }
+}
